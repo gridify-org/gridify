f648759274881d479cd5529d7b83c33fc38e993b
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2024-12-09 16:05:23 +0000

    add fonts

--- frontend/src/app/layout.tsx
+++ frontend/src/app/layout.tsx
@@ -1,6 +1,19 @@
 import type { Metadata } from "next";
+import localFont from "next/font/local";
+
 import "./globals.css";
 import { ThemeProvider } from "@/components/theme-provider";
+
+const geistSans = localFont({
+    src: "./fonts/GeistVF.woff",
+    variable: "--font-geist-sans",
+    weight: "100 900",
+});
+const geistMono = localFont({
+    src: "./fonts/GeistMonoVF.woff",
+    variable: "--font-geist-mono",
+    weight: "100 900",
+});
 
 export const metadata: Metadata = {
     title: "Gridify",
@@ -16,7 +29,9 @@ export default function RootLayout({ children }: RootLayoutProps) {
         <>
             <html lang="en" suppressHydrationWarning>
                 <head />
-                <body>
+                <body
+                    className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+                >
                     <ThemeProvider
                         attribute="class"
                         defaultTheme="system"
