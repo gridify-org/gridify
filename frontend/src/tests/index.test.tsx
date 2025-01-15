d96f559645c34fbab137532dd1aa7be5c77d3ee5
Matthew Mckee <matthewmckee04@yahoo.co.uk>
2025-01-15 23:46:38 +0000

    fix build

--- frontend/src/tests/index.test.tsx
+++ frontend/src/tests/index.test.tsx
@@ -1,7 +1,7 @@
 import { render, screen } from "@testing-library/react";
 import React from "react";
 
-import Home from "./index";
+import Home from "../pages/index";
 
 describe("Home page", () => {
     it("renders Welcome to Gridify text", () => {
